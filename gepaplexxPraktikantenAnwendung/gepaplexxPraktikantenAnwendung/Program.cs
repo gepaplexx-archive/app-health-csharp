4eb5db30243245b63fc26ae86ea3326bc4ef182b
Nico Straßgschwandtner <nico.strassgschwandtner@gepardec.com>
2021-07-05 12:56:56 +0200

    Test of Github

--- gepaplexxPraktikantenAnwendung/gepaplexxPraktikantenAnwendung/Program.cs
+++ gepaplexxPraktikantenAnwendung/gepaplexxPraktikantenAnwendung/Program.cs
@@ -13,6 +13,7 @@ namespace gepaplexxPraktikantenAnwendung
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("test");
             CreateHostBuilder(args).Build().Run();
         }
 
