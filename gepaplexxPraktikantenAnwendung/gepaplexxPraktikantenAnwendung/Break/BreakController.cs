f2131a45ec132fe2c42a4c08029fac37c81fafb7
Jaroslav <jaroslav.romashchuk@gepardec.com>
2021-07-08 11:33:31 +0200

    Break Endpoint Update undone

--- gepaplexxPraktikantenAnwendung/gepaplexxPraktikantenAnwendung/Break/BreakController.cs
+++ gepaplexxPraktikantenAnwendung/gepaplexxPraktikantenAnwendung/Break/BreakController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace gepaplexxPraktikantenAnwendung.Break
 {
@@ -13,11 +16,28 @@ namespace gepaplexxPraktikantenAnwendung.Break
 
             Console.WriteLine("Success");
             string breakText;
+
             
+
+            //Timer pauseFor = new Timer();
+            //pauseFor.Interval = sec * 1000;
+            //pauseFor.Start();
 
             breakText = $"The Application is paused for {sec} seconds";
 
-            return Ok(breakText);
+            var response = HttpContext.Response;
+            response.StatusCode = 423;
+            response.Headers["Retry-After"] = "10";
+           // await response.WriteAsync("Service Unavailable");
+
+            Thread.Sleep(sec * 1000);
+
+            //pauseFor.Stop();
+
+            //response.StatusCode = 200;
+            //waitFor(sec);
+            //response.StatusCode = 200;
+            return BadRequest(breakText);
         }
     }
 }
