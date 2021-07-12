d691a70a41049feb744595ff2961826007a7023f
Jaroslav <jaroslav.romashchuk@gepardec.com>
2021-07-12 11:05:52 +0200

    Unit Test (Health)

--- gepaplexxPraktikantenAnwendung/app-health-csharp-unitTest/UnitTest1.cs
+++ gepaplexxPraktikantenAnwendung/app-health-csharp-unitTest/UnitTest1.cs
@@ -1,12 +1,43 @@
+using gepaplexxPraktikantenAnwendung.AppControllerFolder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace app_health_csharp_unitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        
+        AppController appController = new AppController();
         [TestMethod]
-        public void TestMethod1()
+        public void HealthStatusCode()
+        {
+            OkObjectResult expected = new OkObjectResult(200);
+
+            Task<IActionResult> iaresult = appController.healthapp();
+            OkObjectResult result = (OkObjectResult)iaresult.Result;
+
+            Assert.AreEqual(expected.StatusCode, result.StatusCode);
+        }
+
+        
+        [TestMethod]
+        public void HealthValue()
+        {
+            OkObjectResult expected = new OkObjectResult(200);
+            expected.Value = "Healthy";
+
+            Task<IActionResult> iaresult = appController.healthapp();
+            OkObjectResult result = (OkObjectResult)iaresult.Result;
+
+
+
+            Assert.AreEqual(expected.Value, result.Value);
+            //private const ObjectResult expected = new ObjectResult(Controller.StatusCode(503, "The Application is already terminated"));
+        }
+
+        public void HelloStatusCode()
         {
 
         }
