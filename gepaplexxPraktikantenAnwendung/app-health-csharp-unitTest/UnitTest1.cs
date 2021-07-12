1a2a8241c20e8b91b87affb220484e89b77e00f2
Jaroslav <jaroslav.romashchuk@gepardec.com>
2021-07-12 09:49:20 +0200

    Unit Test Class + references

---
+++ gepaplexxPraktikantenAnwendung/app-health-csharp-unitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace app_health_csharp_unitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
