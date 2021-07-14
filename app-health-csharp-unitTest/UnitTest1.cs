04459bee1e8a7cfe6cd02f2525c5aadf7496a4c2
Jaroslav <jaroslav.romashchuk@gepardec.com>
2021-07-14 11:21:04 +0200

    try to merge from feature/jaroslav to main v2

--- app-health-csharp-unitTest/UnitTest1.cs
+++ app-health-csharp-unitTest/UnitTest1.cs
@@ -30,13 +30,7 @@ namespace app_health_csharp_unitTest
 
             Task<IActionResult> iaresult = appController.healthapp();
             OkObjectResult result = (OkObjectResult)iaresult.Result;
-            
-            
-
-            
-            
-
-
+            OkObjectResult result12 = (OkObjectResult)iaresult.Result;
 
             Assert.AreEqual(expected.Value, result.Value);
             //private const ObjectResult expected = new ObjectResult(Controller.StatusCode(503, "The Application is already terminated"));
