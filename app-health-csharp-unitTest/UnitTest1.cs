24c9097c92527007ad76571d592639617b5c51b7
Nico Straßgschwandtner <nico.strassgschwandtner@gepardec.com>
2021-07-14 10:34:48 +0200

    2. TEst

--- app-health-csharp-unitTest/UnitTest1.cs
+++ app-health-csharp-unitTest/UnitTest1.cs
@@ -58,7 +58,7 @@ namespace app_health_csharp_unitTest
             OkObjectResult expected = new OkObjectResult(200);
             expected.Value = "Hello Max";
 
-            Task<IActionResult> iaresult = appController.helloapp("test");
+            Task<IActionResult> iaresult = appController.helloapp("Max");
             OkObjectResult result = (OkObjectResult)iaresult.Result;
 
 
