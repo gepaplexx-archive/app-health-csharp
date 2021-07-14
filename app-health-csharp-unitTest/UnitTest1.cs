a6962d7bbe815660ef74a63d17a641ebc5320dc5
Jaroslav <jaroslav.romashchuk@gepardec.com>
2021-07-14 11:35:11 +0200

    deleted some tests

--- app-health-csharp-unitTest/UnitTest1.cs
+++ app-health-csharp-unitTest/UnitTest1.cs
@@ -30,8 +30,7 @@ namespace app_health_csharp_unitTest
 
             Task<IActionResult> iaresult = appController.healthapp();
             OkObjectResult result = (OkObjectResult)iaresult.Result;
-            OkObjectResult result123 = (OkObjectResult)iaresult.Result;
-            OkObjectResult result1232 = (OkObjectResult)iaresult.Result;
+            
 
 
 
